7c693504a2eb52e8d95cb5c769c1525c263b54c4
andiiais <andiiais@gmail.com>
2021-05-30 09:35:59 +0700

    [feat] Memoize function

--- src/hooks/useSticky.js
+++ src/hooks/useSticky.js
@@ -1,31 +1,31 @@
-import { useEffect, useState, useRef } from "react"
+import { useEffect, useState, useRef, useCallback } from "react"
 // import { debounce } from "../utils/helper"
 
 function useSticky() {
   const [isSticky, setSticky] = useState(false)
   const element = useRef(null)
 
-  const handleScroll = () => {
+  const handleScroll = useCallback(() => {
     window.scrollY > 100
       ? setSticky(true)
       : setSticky(false)
-  }
+  }, []);
 
-  const debounce = (func, wait = 10, immediate = true) => {
-      let timeOut;
-      return () => {
-        let context = this,
-          args = arguments;
-        const later = () => {
-          timeOut = null;
-          if (!immediate) func.apply(context, args);
-        };
-        const callNow = immediate && !timeOut;
-        clearTimeout(timeOut);
-        timeOut = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
+  const debounce = useCallback((func, wait = 10, immediate = true) => {
+    let timeOut;
+    return () => {
+      let context = this,
+        args = arguments;
+      const later = () => {
+        timeOut = null;
+        if (!immediate) func.apply(context, args);
       };
+      const callNow = immediate && !timeOut;
+      clearTimeout(timeOut);
+      timeOut = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
     };
+  }, []);
 
   useEffect(() => {
     window.addEventListener("scroll", debounce(handleScroll))
