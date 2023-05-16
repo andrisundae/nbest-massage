88c238de347bc3f3350b7c7a8795a7c788392267
adicunk <115680993+adicunk@users.noreply.github.com>
2023-05-16 17:13:53 +0700

    coba test 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -346,7 +346,7 @@ const IndexPage = () => (
                 <p>60 minutes 300K</p>
                 <p>90 minutes 400K</p>
                 <p>120 minutes 550K</p>
-                <div className="mt-4 text-base text-white-2000">Test</div>
+                <div className="px-8 py-5 text-3xl font-bold text-center">Test</div>
                 <p>(Balinese Massage, Javanese Massage, Lomi-lomi Massage, Reflexy, Aromatherapy Massage)</p>
               </div>
             </div>
